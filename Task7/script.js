64689a1189dcfa48d335e42187e1ffc1dd9dcd94
webbyahtisham <ahtishamulhaq384@gmail.com>
2025-06-14 12:37:27 +0500

    add changes iin task7

--- Task7/script.js
+++ Task7/script.js
@@ -1,13 +1,11 @@
 let pro = document.querySelector(".progress")
-let btn = document.querySelector(".button")
-btn.addEventListener("click", function () {
-    let count = 0;
+let count = 0
+function startDownload() {
     let int = setInterval(() => {
-        if (count === 100
-        ) {
+        if (count === 100) {
             clearInterval(int)
         }
         count++
-        pro.style.width = count + "%"
+        pro.style.width = count + "%";
     }, 50);
-})
+}
